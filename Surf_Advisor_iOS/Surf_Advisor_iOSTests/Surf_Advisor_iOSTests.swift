eb339a094c7338c7700831a144cfd3c985591493
박세은 <pkse1213@naver.com>
2019-05-23 19:50:49 +0900

    Project 생성 및 gitignore 추가

---
+++ Surf_Advisor_iOS/Surf_Advisor_iOSTests/Surf_Advisor_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Surf_Advisor_iOSTests.swift
+//  Surf_Advisor_iOSTests
+//
+//  Created by 박세은 on 23/05/2019.
+//  Copyright © 2019 박세은. All rights reserved.
+//
+
+import XCTest
+@testable import Surf_Advisor_iOS
+
+class Surf_Advisor_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
