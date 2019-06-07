6c0d981da6ad6183be646d64b8fc2e1ae19b0e2d
박세은 <pkse1213@naver.com>
2019-06-07 20:40:57 +0900

    해변 시간별 파도 뷰 완료

---
+++ Surf_Advisor_iOS/Surf_Advisor_iOS/Weather/BeachDetailInfoVC.swift
@@ -0,0 +1,30 @@
+//
+//  BeachDetailInfoVC.swift
+//  Surf_Advisor_iOS
+//
+//  Created by 박세은 on 07/06/2019.
+//  Copyright © 2019 박세은. All rights reserved.
+//
+
+import UIKit
+
+class BeachDetailInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "상세기상정보"
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
